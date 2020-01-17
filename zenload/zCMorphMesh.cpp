a2bdbb0edd24398b3049033a90a2373d4763a753
Try <try9998@gmail.com>
2020-01-17 21:44:21 +0100

    initial support for MMID_MMB_ANILIST

--- zenload/zCMorphMesh.cpp
+++ zenload/zCMorphMesh.cpp
@@ -7,10 +7,17 @@
 #include "utils/logger.h"
 #include "vdfs/fileIndex.h"
 
+#include <fstream>
+
 using namespace ZenLoad;
 
-static const uint16_t MMID_MMB_HEADER = 0xE020;
-static const uint16_t MMID_MMB_ANILIST = 0xE030;
+enum MSID_CHUNK : uint16_t {
+  MSID_NONE        = 0,
+  MMID_MMB_HEADER  = 0xE020,
+  MMID_MMB_ANILIST = 0xE030,
+  MMID_UNKNOWN0    = 0xE000,
+  MMID_UNKNOWN1    = 0xE010
+  };
 
 zCMorphMesh::zCMorphMesh(const std::string& fileName, const VDFS::FileIndex& fileIndex)
 {
@@ -38,52 +45,68 @@ zCMorphMesh::zCMorphMesh(const std::string& fileName, const VDFS::FileIndex& fil
 /**
 * @brief Reads the mesh-object from the given binary stream
 */
-void zCMorphMesh::readObjectData(ZenParser& parser)
-{
-    // Information about the whole file we are reading here
-    BinaryFileInfo fileInfo;
+void zCMorphMesh::readObjectData(ZenParser& parser) {
+  // Information about a single chunk
+  BinaryChunkInfo chunkInfo;
 
-    // Information about a single chunk
-    BinaryChunkInfo chunkInfo;
+  // Read chunks until we left the virtual binary file or got to the end-chunk
+  // Each chunk starts with a header (BinaryChunkInfo) which gives information
+  // about what to do and how long the chunk is
+  bool doneReadingChunks = false;
+  while (!doneReadingChunks) {
+    // Read chunk header and calculate position of next chunk
+    parser.readStructure(chunkInfo);
 
-    // Read chunks until we left the virtual binary file or got to the end-chunk
-    // Each chunk starts with a header (BinaryChunkInfo) which gives information
-    // about what to do and how long the chunk is
-    bool doneReadingChunks = false;
-    while (!doneReadingChunks)
-    {
-        // Read chunk header and calculate position of next chunk
-        parser.readStructure(chunkInfo);
+    size_t chunkEnd = parser.getSeek() + chunkInfo.length;
 
-        size_t chunkEnd = parser.getSeek() + chunkInfo.length;
+    switch (chunkInfo.id) {
+      case MMID_MMB_HEADER: {
+        uint32_t version = parser.readBinaryDWord();
 
-        switch (chunkInfo.id)
-        {
-            case MMID_MMB_HEADER:
-            {
-                uint32_t version = parser.readBinaryDWord();
+        std::string morphProtoName = parser.readLine(true);
+        //LogInfo() << "MorphProtoName: " << morphProtoName;
 
-                std::string morphProtoName = parser.readLine(true);
-                //LogInfo() << "MorphProtoName: " << morphProtoName;
+        // Read source-mesh
+        m_Mesh.readObjectData(parser);
 
-                // Read source-mesh
-                m_Mesh.readObjectData(parser);
-
-                // TODO: use these
-                std::vector<ZMath::float3> morphPositions(m_Mesh.getVertices().size());
-                parser.readBinaryRaw(morphPositions.data(), sizeof(ZMath::float3) * m_Mesh.getVertices().size());
-            }
-            break;
-
-            case MMID_MMB_ANILIST:
-                doneReadingChunks = true;
-                parser.setSeek(chunkEnd);
-                break;
-
-                // Morphmeshes don't have a real end-tag. The anilist is the last one, however
-
-            default:
-                parser.setSeek(chunkEnd);  // Skip chunk
+        // TODO: use these
+        std::vector<ZMath::float3> morphPositions(m_Mesh.getVertices().size());
+        parser.readBinaryRaw(morphPositions.data(), sizeof(ZMath::float3) * m_Mesh.getVertices().size());
+        break;
         }
+
+      case MMID_MMB_ANILIST: {
+        doneReadingChunks = true;
+        uint16_t aniTotal=0;
+        parser.readBinaryRaw(&aniTotal,2);
+        aniList.resize(aniTotal);
+        for(auto& i:aniList) {
+          i.name = parser.readString(false);
+          parser.setSeek(parser.getSeek()+22); // unknown data
+
+          uint32_t indexSz=parser.readBinaryDWord();
+          i.numFrames = parser.readBinaryDWord();
+
+          i.vertexIndex.resize(indexSz);
+          i.samples.resize(i.numFrames*indexSz);
+          parser.readBinaryRaw(i.vertexIndex.data(),indexSz*4);
+          static_assert(sizeof(i.samples[0])==12,"invalid ani sample size");
+          parser.readBinaryRaw(i.samples.data(),i.numFrames*indexSz*12);
+          }
+
+        parser.setSeek(chunkEnd);
+        break;
+        }
+
+      case MMID_UNKNOWN0:
+      case MMID_UNKNOWN1: {
+        parser.setSeek(chunkEnd);
+        break;
+        }
+
+      // Morphmeshes don't have a real end-tag. The anilist is the last one, however
+      default:
+        parser.setSeek(chunkEnd);  // Skip chunk
+      }
     }
-}
+  }
--- zenload/zCMorphMesh.h
+++ zenload/zCMorphMesh.h
@@ -6,37 +6,45 @@
 
 namespace VDFS
 {
-    class FileIndex;
+class FileIndex;
 }
 
 namespace ZenLoad
 {
-    class ZenParser;
-    class zCMorphMesh
-    {
-    public:
-        zCMorphMesh() {}
+class ZenParser;
+class zCMorphMesh {
+  public:
+    struct Animation {
+      std::string                name;
+      std::vector<uint32_t>      vertexIndex;
+      std::vector<ZMath::float3> samples;
+      uint32_t                   numFrames=0;
+      };
 
-        /**
-		 * @brief Loads the mesh from the given VDF-Archive
-		 */
-        zCMorphMesh(const std::string& fileName, const VDFS::FileIndex& fileIndex);
+    zCMorphMesh() = default;
 
-        /**
-         * @brief Reads the mesh-object from the given binary stream
-         * @param fromZen Whether this mesh is supposed to be read from a zenfile. In this case, information about the binary chunk is also read.
-         */
-        void readObjectData(ZenParser& parser);
+    /**
+     * @brief Loads the mesh from the given VDF-Archive
+     */
+    zCMorphMesh(const std::string& fileName, const VDFS::FileIndex& fileIndex);
 
-        /**
-         * @return Internal zCProgMeshProto of this soft skin. The soft-skin only displaces the vertices found in the ProgMesh.
-         */
-        const zCProgMeshProto& getMesh() const { return m_Mesh; }
+    /**
+     * @brief Reads the mesh-object from the given binary stream
+     * @param fromZen Whether this mesh is supposed to be read from a zenfile. In this case, information about the binary chunk is also read.
+     */
+    void readObjectData(ZenParser& parser);
 
-    private:
-        /**
-         * @brief Internal zCProgMeshProto of this soft skin. The soft-skin only displaces the vertices found in the ProgMesh.
-         */
-        zCProgMeshProto m_Mesh;
-    };
+    /**
+     * @return Internal zCProgMeshProto of this soft skin. The soft-skin only displaces the vertices found in the ProgMesh.
+     */
+    const zCProgMeshProto& getMesh() const { return m_Mesh; }
+
+    std::vector<Animation> aniList;
+
+  private:
+    /**
+     * @brief Internal zCProgMeshProto of this soft skin. The soft-skin only displaces the vertices found in the ProgMesh.
+     */
+    zCProgMeshProto m_Mesh;
+  };
 }  // namespace ZenLoad
